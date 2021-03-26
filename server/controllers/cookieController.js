e8747df0261398901dcb01b1d20c0320be4d98c3
Newas <newas.azad@bjss.com>
2021-03-25 20:18:04 -0400

    added cookie controller: setting cookie and removing cookie

---
+++ server/controllers/cookieController.js
@@ -0,0 +1,18 @@
+const cookieController = {};
+
+/* setSSIDCookie - store the user id in a cookie */
+cookieController.setSSIDCookie = (req, res, next) => {
+    res.cookie('ssid', res.locals.userId, {
+        httpOnly: true
+    });
+    return next();
+}
+
+/* clearCookie - deletes cookie locally */
+cookieController.removeCookie = (req, res, next) => {
+    res.locals.cookie = req.cookies.ssid;
+    res.clearCookie('ssid');
+    return next();
+}
+
+module.exports = cookieController;
