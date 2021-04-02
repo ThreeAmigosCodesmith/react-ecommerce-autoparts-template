dbe4800fc061c738e3140a8d36a16e6e17b977a2
Newas <newas.azad@bjss.com>
2021-04-01 22:05:23 -0400

    implemented password hashing

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,11 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-else-return */
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
 const mongoose = require('mongoose');
+
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,6 +16,22 @@ const UserSchema = new mongoose.Schema({
   products: { type: Object },
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function (next) {
+  const user = this;
 
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (saltError, salt) {
+      if (saltError) return next(saltError);
+      else {
+        bcrypt.hash(user.password, salt, function(hashError, hash) {
+          if (hashError) return next(hashError);
+          user.password = hash;
+          return next();
+        });
+      }
+    });
+  } else return next();
+});
+
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
