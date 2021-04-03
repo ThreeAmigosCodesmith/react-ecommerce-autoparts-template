bee89ef416f51017ff6f62c7f4eab982518ad28f
Newas <newas.azad@bjss.com>
2021-04-03 11:09:30 -0400

    minor ui changes

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,9 +1,22 @@
 import './Sidebar.css';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Sidebar = () => {
-  // eslint-disable-next-line no-console
-  console.log('Sidebar');
+  const history = useHistory();
+
+  const logOutButton = (event) => {
+    event.preventDefault();
+    fetch('/api/verify', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status === 200) history.push('/');
+    });
+  };
+
   return (
     <div id="sidebar">
 
@@ -11,25 +24,23 @@ const Sidebar = () => {
         <div className="sidebar__link active_menu_link">
           <a href="/dashboard">Dashboard</a>
         </div>
-        <h2>MNG</h2>
         <div className="sidebar__link">
-          <a href="#test">Admin Management</a>
+          <a href="/dashboard/newProduct">New Listing</a>
         </div>
         <div className="sidebar__link">
-          <a href="/dashboard/newProduct">List a New Product</a>
+          <a href="/inventory">Inventory</a>
         </div>
         <div className="sidebar__link">
-          <a href="#test">Log out</a>
-        </div>
-        <h2>LEAVE</h2>
-        <div className="sidebar__link">
-          <a href="#test">Log out</a>
+          <a href="/purchases">Purchases</a>
         </div>
         <div className="sidebar__link">
-          <a href="#test">Log out</a>
+          <a href="/messages">Messages</a>
+        </div>
+        <div className="sidebar__link">
+          <a href="/settings">Settings</a>
         </div>
         <div className="sidebar__logout">
-          <a href="#test">Log out</a>
+          <a href="/" type="button" onClick={(e) => logOutButton(e)}>Log out</a>
         </div>
       </div>
     </div>
