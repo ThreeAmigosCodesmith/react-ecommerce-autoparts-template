bffd73a170ef5b7e965852e52bf3a41d8b07e1eb
Newas <newas.azad@bjss.com>
2021-04-03 13:09:13 -0400

    checkout functionality works minimalistically

--- src/components/Subtotal/Subtotal.jsx
+++ src/components/Subtotal/Subtotal.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from '../../StateProvider';
 
+const moment = require('moment');
+
 const Subtotal = () => {
-  const [{ cart }] = useStateValue();
+  const [{ cart, user }] = useStateValue();
+  const history = useHistory();
+
+  const validateCheckOut = () => {
+    if (cart.length < 1) return false;
+    return true;
+  };
+
+  const checkoutButton = (event) => {
+    event.preventDefault();
+    fetch('/api/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        date: moment.now(),
+        productId: '0000000000',
+        sellerId: '0934977284',
+        buyerId: user.id,
+      }),
+    }).then((res) => {
+      if (res.status === 200) history.push('/confirmation');
+    })
+      // eslint-disable-next-line no-console
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className="subtotal">
@@ -27,7 +56,7 @@ const Subtotal = () => {
         thousandSeparator
         prefix="$"
       />
-      <button type="button">Proceed to Checkout</button>
+      <button type="button" disabled={!validateCheckOut()} onClick={(e) => checkoutButton(e)}>Proceed to Checkout</button>
     </div>
   );
 };
