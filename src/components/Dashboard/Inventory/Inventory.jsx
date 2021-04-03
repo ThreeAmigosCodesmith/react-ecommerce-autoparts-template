e9fb253c4496f440b0ff0eb0afe339bc2da636d2
Newas <newas.azad@bjss.com>
2021-04-03 11:25:59 -0400

    created jsx and css files for new components in dashboard page

---
+++ src/components/Dashboard/Inventory/Inventory.jsx
@@ -0,0 +1,15 @@
+import './Inventory.css';
+import React from 'react';
+
+const Inventory = () => {
+  // eslint-disable-next-line no-console
+  console.log('placeholder');
+
+  return (
+    <div id="inventory">
+      <h1>Current Inventory</h1>
+    </div>
+  );
+};
+
+export default Inventory;
---
+++ src/components/Dashboard/Purchases/Purchases.jsx
@@ -0,0 +1,15 @@
+import './Purchases.css';
+import React from 'react';
+
+const Purchases = () => {
+  // eslint-disable-next-line no-console
+  console.log('placeholder');
+
+  return (
+    <div id="purchases">
+      <h1>Purchases History</h1>
+    </div>
+  );
+};
+
+export default Purchases;
