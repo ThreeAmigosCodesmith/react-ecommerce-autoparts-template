9869fe1b381d2da32c303cb84a873b70aff7f966
Ashley Pean <50483172+ashleypean@users.noreply.github.com>
2021-04-07 22:53:39 -0400

    Update database.js
    remove secret keys

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize(process.env.CONNECTION_STRING || 'postgres://epbidatr:eCOCgYtbWofU0n9G07NFS1vxa-ulPXEh@queenie.db.elephantsql.com:5432/epbidatr', {
+module.exports = new Sequelize(process.env.CONNECTION_STRING, {
   dialect: 'postgres',
 });
