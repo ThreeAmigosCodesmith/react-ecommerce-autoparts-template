f3ca25cc61a2bcf7372d45698dbb7eb5bb420410
Ashley Pean <pean.ashley@gmail.com>
2021-04-14 22:26:42 -0400

    header fully responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SearchIcon from '@material-ui/icons/Search';
+// import SearchIcon from '@material-ui/icons/Search';
 import Nav from '../Nav/Nav';
 import Logo from '../../yardhop-logo.png';
 import './Header.css';
@@ -16,14 +16,14 @@ const Header = () => (
 
     <div className="header__search">
       <Nav />
-      <div className="header__searchbar">
+      {/* <div className="header__searchbar">
         <input
           type="text"
           className="header__searchInput"
           placeholder="Search for car parts using keywords or #..."
         />
         <SearchIcon className="header__searchIcon" />
-      </div>
+      </div> */}
     </div>
 
   </div>
