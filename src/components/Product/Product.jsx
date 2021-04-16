43a1ea26755f6d4ff0bee73714694bd81278ad77
Sam Filip <samjfilip@gmail.com>
2021-04-15 19:14:00 -0700

    removed unneccessary code;

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,8 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import './Product.css';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import * as actions from '../../redux/actions/actionTypes';
 
 const Product = (props) => {
   const dispatch = useDispatch();
@@ -16,13 +13,13 @@ const Product = (props) => {
     image,
     location,
     condition,
-    supplierID,
   } = props;
 
   const addToCart = () => {
     // dispatch item to the data layer
+    // eslint-disable-next-line no-console
     dispatch({
-      type: actions.ADD_TO_CART,
+      type: 'ADD_TO_CART',
       item: {
         id,
         title,
@@ -30,19 +27,6 @@ const Product = (props) => {
         price,
         location,
         condition,
-      },
-    });
-  };
-
-  const messageSeller = () => {
-    const chatSessionID = uuidv4();
-    dispatch({
-      type: actions.START_CHAT,
-      payload: {
-        supplierID,
-        chatSessionID,
-        productID: id,
-        title,
       },
     });
   };
@@ -58,14 +42,13 @@ const Product = (props) => {
         <p>{`Condition: ${condition}`}</p>
         <p className="product__location">
           <LocationOnIcon />
-          <span>{location}</span>
+          <span>{location.borough}</span>
         </p>
       </div>
       <div className="product__image">
         <img src={image} alt="" />
       </div>
       <button type="button" onClick={addToCart}>Add to Cart</button>
-      <button type="button" onClick={messageSeller}>Message Seller</button>
     </div>
   );
 };
@@ -76,13 +59,7 @@ Product.propTypes = {
   price: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
   condition: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  // eslint-disable-next-line react/require-default-props
-  supplierID: PropTypes.string,
+  location: PropTypes.shape({ borough: PropTypes.string.isRequired }).isRequired,
 };
-
-// supplierID.defaultProps = {
-//   name: '',
-// };
 
 export default Product;
