03fa2968af03f903e42251d1a1c8e5ff8ff6463b
Ashley Pean <pean.ashley@gmail.com>
2021-04-15 20:08:04 -0400

    added dashboard table

--- src/components/Dashboard/Messages/Messages.jsx
+++ src/components/Dashboard/Messages/Messages.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-use-before-define */
 import './Messages.css';
 import React from 'react';
+import MaterialTable from 'material-table';
 
 const Messages = () => {
   // eslint-disable-next-line no-console
@@ -8,8 +10,139 @@ const Messages = () => {
   return (
     <div id="messages">
       <h1>Current Messages</h1>
+      <MaterialTable style={tableStyle} title="Message History" data={messages} columns={columns} />
     </div>
   );
 };
+
+const tableStyle = {
+  width: '100%',
+};
+
+const columns = [
+  {
+    title: 'Chat ID',
+    field: 'chatID',
+  },
+  {
+    title: 'Customer Name',
+    field: 'customerName',
+  },
+  {
+    title: 'Customer Phone',
+    field: 'customerPhone',
+  },
+  {
+    title: 'Customer Email',
+    field: 'customerEmail',
+  },
+  {
+    title: 'Active?',
+    field: 'active',
+  },
+  {
+    title: 'Chat Link',
+    field: 'chatLink',
+  },
+];
+
+const messages = [
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    customerPhone: '123-456-7890',
+    customerEmail: 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+  {
+    chatID: '12345',
+    customerName: 'Ashley Pean',
+    'Customer Phone': '123-456-7890',
+    'Customer Email': 'test@test.com',
+    active: true,
+    chatLink: 'link.link',
+  },
+];
 
 export default Messages;
