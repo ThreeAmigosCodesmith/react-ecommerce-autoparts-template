259661201c97282a3534ac2a53b53509f9b2f426
Ashley Pean <pean.ashley@gmail.com>
2021-04-15 20:55:46 -0400

    basic layout for chat page

--- src/components/Dashboard/Messages/Messages.jsx
+++ src/components/Dashboard/Messages/Messages.jsx
@@ -1,16 +1,34 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
 import './Messages.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import MaterialTable from 'material-table';
+import Modal from '@material-ui/core/Modal';
+import Chat from '../../Chat/Chat';
+import * as types from '../../../redux/actions/actionTypes';
 
 const Messages = () => {
   // eslint-disable-next-line no-console
   console.log('placeholder');
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleChat = (e, rowData) => {
+    console.log(rowData);
+    setModalOpen(true);
+    // dispatch({ type: types. })
+  };
 
   return (
     <div id="messages">
-      <h1>Current Messages</h1>
-      <MaterialTable style={tableStyle} title="Message History" data={messages} columns={columns} />
+      <h1>Message History</h1>
+      <h4>Current Chats</h4>
+      <MaterialTable style={tableStyle} title="" data={messages} columns={columns} onRowClick={handleChat} />
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+        <Chat />
+      </Modal>
+      <h4>Past Chats</h4>
+      <MaterialTable style={tableStyle} title="" data={messages} columns={columns} />
     </div>
   );
 };
