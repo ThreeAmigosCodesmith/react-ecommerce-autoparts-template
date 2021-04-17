5ff6af7e1b4b9dccceecb0dfa3a3301801a5b5c4
Ashley Pean <pean.ashley@gmail.com>
2021-04-17 03:40:50 -0400

    catalog UI set up

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
@@ -41,11 +42,23 @@ const styles = {
 
 function Catalog() {
   const products = useSelector((state) => state.products.catalog);
-  console.log('loaded', products);
   const dispatch = useDispatch();
-  const conditions = ['Good', 'Bad', 'Decent', 'New'];
-  const colors = ['Red', 'Blue', 'Green', 'Purple', 'Orange'];
+  // const conditions = ['Good', 'Bad', 'Decent', 'New'];
+  // const colors = ['Red', 'Blue', 'Green', 'Purple', 'Orange'];
   const [searchText, setSearchText] = useState('');
+  const [conditions, setConditions] = useState({
+    Good: true,
+    Bad: true,
+    Decent: true,
+    New: true,
+  });
+  const [colors, setColors] = useState({
+    Red: true,
+    Blue: true,
+    Green: true,
+    Purple: true,
+    orange: true,
+  });
   const [checkbox, setCheckbox] = useState({
     Good: true,
     Bad: true,
@@ -68,6 +81,7 @@ function Catalog() {
           price: el.price,
           images: el.images,
           location: 'Queens',
+          colors: ['Red', 'Blue', 'Green', 'Purple', 'Orange'],
           condition: el.condition,
           supplierID: el.supplierID,
         }));
@@ -78,13 +92,20 @@ function Catalog() {
     getAllProducts();
   }, []);
 
-  const handleChange = (e) => {
+  const handleCondition = (e, condition) => {
     const { target: { checked, name } } = e;
-    setCheckbox({ ...checkbox, [name]: checked });
+    console.log(checked, name);
+    setConditions({ ...conditions, [name]: checked });
   };
 
   const handleSearch = (e) => {
-    setSearchText(e.target.value);
+    console.log(e.target.value);
+    // setSearchText(e.target.value);
+  };
+
+  const handleColor = (e) => {
+    const { target: { checked, name } } = e;
+    console.log(checked, name);
   };
 
   return (
@@ -101,12 +122,13 @@ function Catalog() {
             <FormControl component="fieldset">
               <FormLabel style={styles.formLabel} component="legend">Condition</FormLabel>
               <FormGroup>
-                {conditions.map((condition) => (
+                {Object.keys(conditions).map((condition) => (
                   <FormControlLabel
+                    key={uuidv4()}
                     control={(
                       <Checkbox
-                        checked={checkbox[condition]}
-                        onChange={handleChange}
+                        checked={conditions[condition]}
+                        onChange={(e) => handleCondition(e, condition)}
                         name={condition}
                         style={styles.conditionButton}
                       />
@@ -120,12 +142,13 @@ function Catalog() {
             <FormControl component="fieldset">
               <FormLabel style={styles.formLabel} component="legend">Color</FormLabel>
               <FormGroup>
-                {colors.map((color) => (
+                {Object.keys(colors).map((color) => (
                   <FormControlLabel
+                    key={uuidv4()}
                     control={(
                       <Checkbox
-                        checked={checkbox[color]}
-                        onChange={handleChange}
+                        checked={colors[color]}
+                        onChange={(e) => handleColor(e, color)}
                         name={color}
                         style={{ color }}
                       />
@@ -140,7 +163,7 @@ function Catalog() {
 
         <div className="home_productsNearby">
           {products.length > 0 ? products.map((item) => (
-            <div style={{ width: '450px' }}>
+            <div style={{ width: '450px' }} key={uuidv4()}>
               <Product
                 id={item.id}
                 title={item.title}
@@ -153,102 +176,10 @@ function Catalog() {
                 location={item.location}
               />
             </div>
-          )) : fallback}
+          )) : 'No products found'}
         </div>
       </div>
     </div>
   );
 }
 export default Catalog;
-
-const fallback = (
-  <>
-    <div className="home__row">
-      <Product
-        id="0000000000"
-        title="OE Ford 2007 Mustang Part LH Left Driver Tail Light Lamp Assembly Taillight"
-        price={96.98}
-        images={['https://i.pinimg.com/originals/3b/21/ae/3b21aeed0898505c1b02cf56aee15ab5.jpg']}
-        condition="Good"
-        location="Queens"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000001"
-        title="2016 2017 2018 2019 2020 Chevrolet Camaro ZL1 Front Bumper Cover OEM"
-        price={445.98}
-        images={['https://i.ebayimg.com/images/g/iIUAAOSwo0RfDJJq/s-l300.jpg']}
-        condition="Good"
-        location="Brooklyn"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000002"
-        title="2nd Row Seat 2015-2019 Chevy Chevrolet Tahoe Yukon Cadillac Escalade"
-        price={246.98}
-        images={['https://i.ebayimg.com/thumbs/images/g/Wg8AAOSwp8Jcp5~o/s-l300.jpg']}
-        condition="Excellent"
-        location="Bronx"
-        supplierID="12345"
-      />
-    </div>
-    <div className="home__row">
-      <Product
-        id="0000000003"
-        title="2014-2018 GMC Sierra Denali 1500 Engine Motor 6.2L Vin J 8th Digit Option L86"
-        price={650.93}
-        images={['https://i.ebayimg.com/images/g/s8oAAOSw5ClXxKIF/s-l300.jpg']}
-        condition="Good"
-        location="Bronx"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000004"
-        title="2017-2018 Hyundai Elantra Left Driver Side Door Mirror 87610F3050S20"
-        price={378.99}
-        images={['https://i.ebayimg.com/images/g/7tYAAOSwIGlekGdz/s-l640.jpg']}
-        condition="Good"
-        location="Queens"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000005"
-        title="2014 Volkswaggen Jetta Hood"
-        price={180.75}
-        images={['https://store.nurburgringautoparts.com/media/catalog/product/153/720/153720_04.jpg']}
-        condition="Good"
-        location="Brooklyn"
-        supplierID="12345"
-      />
-    </div>
-    <div className="home__row">
-      <Product
-        id="0000000006"
-        title="2014 Porsche Cayenne Display Screen Navigation GPS Radio"
-        price={699.98}
-        images={['https://d27z5xsz0y1qnm.cloudfront.net/benzeen/large/436429_01.jpg']}
-        condition="Good"
-        location="Queens"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000007"
-        title="2016 2017 2018 2019 2020 Honda Civic Sedan Front Left Side Driver Door OEM"
-        price={389.99}
-        images={['https://i.ebayimg.com/thumbs/images/g/EOAAAOSw3RlgKZSR/s-l300.jpg']}
-        condition="Good"
-        location="Brooklyn"
-        supplierID="12345"
-      />
-      <Product
-        id="0000000008"
-        title="2017 2018 2019 Mini Cooper Countryman Left Driver LED Headlight"
-        price={273.95}
-        images={['https://i.ebayimg.com/images/g/dtoAAOSwsBtaN8UQ/s-l640.jpg']}
-        condition="Good"
-        location="Staten Island"
-        supplierID="12345"
-      />
-    </div>
-  </>
-);
